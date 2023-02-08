2c31ae8e4cb6eae2da7b48df54e3f5f6ddeca9ab
Mario <mariovictorino31@gmail.com>
2023-02-08 13:31:19 -0800

    Add project files.

---
+++ Shutdown After Time C++ Version/Shutdown After Time C++ Version.cpp
@@ -0,0 +1,42 @@
+// Shutdown After Time C++ Version.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <ctype.h>
+#include <process.h>
+#include <string>
+
+using namespace std;
+
+int tm;
+string output;
+string x;
+
+bool run = true;
+
+int main()
+{
+    cout << "Enter time to shutdown in seconds or press q to quit" << endl;
+    cout << "1 Hour = 3600 | 2 Hours = 7200 | 3 Hours 10800 | 4 Hours = 14400" << endl;
+
+    while (run) {
+        cin >> tm;
+        if (isdigit(tm)) {
+            if (tm >= 0 || tm <= 28800) {
+                output = ("shutdown /s /t " + tm);
+                system(output);
+            }
+        }
+        else if (!(isdigit(tm))) {
+            if ((x = to_string(tm)) == "q"){
+                run = false;
+            }
+            else {
+                cout << "Invalid Submission";
+            }
+        }
+    }
+}
+
+
