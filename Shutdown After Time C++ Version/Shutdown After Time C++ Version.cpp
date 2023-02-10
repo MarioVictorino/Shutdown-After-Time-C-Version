1f196bd13bf487af637a479ee9869fa9b6fd1d24
Mario <mariovictorino31@gmail.com>
2023-02-09 16:53:52 -0800

    Updated input check

--- Shutdown After Time C++ Version/Shutdown After Time C++ Version.cpp
+++ Shutdown After Time C++ Version/Shutdown After Time C++ Version.cpp
@@ -9,9 +9,13 @@
 
 using namespace std;
 
-int tm;
+string tm;
 string output;
 string x;
+
+int approvedNum;
+
+bool check;
 
 bool run = true;
 
@@ -22,21 +26,51 @@ int main()
 
     while (run) {
         cin >> tm;
+
+        for (int i = 0; i < tm.length(); i++) {
+            if (isdigit(tm[i]) == false) {
+                check = false;
+            }
+            else {
+                check = true;
+                approvedNum = stoi(tm);
+            }
+        }
+
+        if (check == true) {
+            if (approvedNum >= 0 && approvedNum <= 28800) {
+                output = " shutdown /s /t " + approvedNum;
+                cout << output;
+                system(static_cast<const char*>(output.c_str()));
+                return 0;
+            }
+        }
+        else if (check == false) {
+            if (tm == "q") {
+                run = false;
+                return 0;
+            }
+            else {
+                cout << "Invalid Submission" << endl;
+            }
+        }
+        /*
         if (isdigit(tm)) {
             if (tm >= 0 && tm <= 28800) {
-                output = ("shutdown /s /t " + tm);
-                system(output);
+                output = "shutdown /s /t " + tm;
+                system(static_cast<const char *>(output.c_str()));
+                return 0;
             }
         }
         else if (!(isdigit(tm))) {
             if ((x = to_string(tm)) == "q"){
                 run = false;
-                return;
+                return 0;
             }
             else {
-                cout << "Invalid Submission";
+                cout << "Invalid Submission" << endl;
             }
-        }
+        } */
     }
 }
 
